2f474a0884265e815f6b2aaa26a4c21d3862d1f1
Fernanda Neto <fernanda_rneto@hotmail.com>
2022-12-14 02:47:31 -0300

    B8ZS added

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from Crypto import Vegenere
+from Vegenere import Vegenere
+from LineCode import B8ZS
 
 if __name__ == "__main__":
     crypt = Vegenere()
-    crypt.encodeVegenere('opa braÇo?')
+    b8 = B8ZS()
+    crypt.encodeVegenere('Y!')
     print(crypt.getCryptographedMessage())
     a = crypt.convertBinary()
+
+    print(b8.encode(a))
+
     s = ''.join([str(item) for item in a])
     print(s)
     crypt.decodeVegenere(a)
     print(crypt.convertToString())
     print(crypt.getDecryptographedMessage())
+    
